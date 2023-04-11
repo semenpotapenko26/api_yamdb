74fec4104f9da7e10db934e8ff5d69365a816c68
Татьяна Туковская <ttykovskaya@bk.ru>
2023-04-12 00:06:27 +0300

    Models-review, comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,9 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
+
+class User():
+    pass
 
 
 class Category(models.Model):
@@ -39,3 +44,68 @@ class Genre_Title(models.Model):
                                  on_delete=models.SET_NULL,
                                  null = True,
                                  )
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор отзыва',
+    )
+    text = models.TextField('Текст отзыва')
+    score = models.PositiveSmallIntegerField(
+        'Рейтинг',
+        validators=[
+            MinValueValidator(1, 'Минимально 1',),
+            MaxValueValidator(10, 'Максимально 10',)
+        ],
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        constraints = (
+            models.UniqueConstraint(
+                fields=['title', 'author'],
+                name='author_title_unique'
+            ),
+        )
+
+    def __str__(self):
+        return self.text[:15]
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв',
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор комментария'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text[:15]
