53f3ff1d2164b55e558a638e638d68ef14e33d9c
Stryukov <stryukov.semen@gmail.com>
2023-04-14 05:45:07 +1200

    Added user model, viewset, urls and serializer

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 
 from .models import User
 from .serializers import UserSerializer
@@ -15,8 +16,19 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     lookup_field = 'username'
 
-    @action(methods=['get', 'put', 'patch'], detail=True, url_path='me')
-    def me(self, request, username):
+    @action(methods=['get', 'patch'], detail=True)
+    def me(self, request):
         serializer = self.get_serializer_class()
-        data = serializer(request.user).data
-        return Response(data, status=status.HTTP_200_OK)
+        user = get_object_or_404(User, username=request.user.username)
+        if self.request.method == 'PATCH':
+            serializer = serializer(user, request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    serializer.data, status=status.HTTP_200_OK
+                )
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+        serializer = serializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
