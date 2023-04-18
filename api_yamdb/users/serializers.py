ac3e00cff50297692063853f07b192bb04f35282
Stryukov <stryukov.semen@gmail.com>
2023-04-19 09:53:41 +1200

    Fixed some bugs

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -1,3 +1,4 @@
+import re
 from rest_framework import serializers
 from django.contrib.auth.tokens import default_token_generator
 from django.shortcuts import get_object_or_404
@@ -15,6 +16,18 @@ class UserSerializer(serializers.ModelSerializer):
     def validate_username(self, value):
         if value.lower() == 'me':
             raise serializers.ValidationError('Нельзя использовать имя me')
+        return value
+
+
+class SelfCreateUserSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    email = serializers.EmailField(max_length=254)
+
+    def validate_username(self, value):
+        if value.lower() == 'me':
+            raise serializers.ValidationError('Нельзя использовать имя me')
+        # if not re.match("^[\w.@+-]+\z", value):
+        #     raise serializers.ValidationError('Не используйте спец символы')
         return value
 
 
