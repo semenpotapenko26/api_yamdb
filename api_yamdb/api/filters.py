ca1cbd2dbe6f19ef68b1cf0b0ff8f4960187eaf5
Семён Потапенко <sempotapenko@yandex.ru>
2023-04-20 00:25:56 +0400

    Fix bugs

---
+++ api_yamdb/api/filters.py
@@ -0,0 +1,12 @@
+from django_filters import CharFilter, FilterSet, NumberFilter
+from reviews.models import Title
+
+
+class TitleFilter(FilterSet):
+    genre = CharFilter(field_name='genre__slug', lookup_expr='icontains')
+    category = CharFilter(field_name='category__slug', lookup_expr='icontains')
+    name = CharFilter(field_name='name', lookup_expr='icontains')
+    year = NumberFilter(field_name='year', lookup_expr='exact')
+    class Meta:
+        model = Title
+        exclude = ('id', 'description',)
